fa229902ec746dfb3fec54d2a0ee3e2e97a13235
Sebastian Johnsson <sebastian@agiley.se>
2015-05-31 16:34:30 +0200

    Update config

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,6 @@ normal[:squid][:port]                     =   8800
 normal[:squid][:pid_file]                 =   '/var/run/squid3.pid'
 
 normal[:squid][:mode]                     =   'proxy'
-normal[:squid][:enable_cache_dir]         =   false
 
 normal[:squid][:dns][:nameservers]        =   ['8.8.8.8', '8.8.4.4']
 normal[:squid][:dns][:ipcache_size]       =   10240
