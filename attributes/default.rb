6c9a32cc8f3320bbdad8b5861c0bccebdda7de63
Sebastian Johnsson <sebastian@agiley.se>
2015-05-31 22:14:11 +0200

    Update logging management

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,8 +5,8 @@ normal[:squid][:pid_file]                 =   '/var/run/squid3.pid'
 
 normal[:squid][:access_log_path]          =   "#{node[:squid][:log_dir]}/access.log"
 normal[:squid][:cache_log_path]           =   "#{node[:squid][:log_dir]}/cache.log"
-normal[:squid][:logging_protocol]         =   "daemon"
-normal[:squid][:logging_mode]             =   "combined"
+normal[:squid][:log_module]               =   "daemon"
+normal[:squid][:log_format]               =   "common"
 
 normal[:squid][:dns][:nameservers]        =   ['8.8.8.8', '8.8.4.4']
 normal[:squid][:dns][:ipcache_size]       =   10240
